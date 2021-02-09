8e43d5d1299ecbd4d6dbaa8e68f392122e1f1a7f
errolkutan <errol.kutan@mongodb.com>
2021-02-09 09:59:52 -0600

    Sleep 3 seconds when collecting threaddump in getstacktraces.sh; short-circuit if PID cannot be found

--- scripts/getstacktraces.sh
+++ scripts/getstacktraces.sh
@@ -7,11 +7,16 @@ fi
 
 PID=$(ps -ef | grep java | head -n 1 | grep couch-to-mongo | awk '{ print $2 }')
 #PID=$(ps -ef | grep java | head -n 1 | awk '{ print $2 }')
-echo "PID is $PID"
 
-echo "Collecting $NUM_RUNS thread dumps"
+if [ $PID = ""]; then
+    echo "Could not find a PID for couch-to-mongo process. Terminating"
+    exit 1
+fi
+
+echo "Collecting $NUM_RUNS thread dumps from PID $PID"
 for i in {1..$NUM_RUNS}
 do
     jstack -F -l $PID >> "stacktrace.$PID.$i.txt";
+    sleep 3;
 done
 
